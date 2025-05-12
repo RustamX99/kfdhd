adde38deecb012cdc38e3e83127d296691af95f5
RustamX99 <qodirabduqodir47@gmail.com>
2025-05-12 09:59:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// var yosh = 'ism:', ism;
+
+// var ism = prompt('Ismingizni kiriting');
+
+
+// // console.log(ism, yosh);
+
+// alert('Salom men meing ismim Abduqodir va men sizga ozim haqida malomat beraman');
+
+// let ism = prompt('Ismingizni kiriting');
+
+// console.log('ism:' , ism);
+
+// let yosh = prompt('Yoshingizni kiriting');
+// console.log('yosh:', yosh);
+
+// let kasbingiz = prompt('Kasbingizni kiriting');
+// console.log('kasb:', kasbingiz);
+
+// let manzil = prompt('Manzilingizni kiriting');
+// console.log('manzil:', manzil);
+
+// let tel = prompt('Telefon raqamingizni kiriting');
+// console.log('tel:', tel);
+
+// alert('Salom ' + ' men ' + ism + ' ' + ' men ' + yosh + 'yoshdaman' + ' men ' + kasbingiz + ' manzil ' + manzil + ' telefon raqamingiz ' + tel );
+
+let ism = prompt('Ismingizni kiriting');
+let familiya = prompt('Familiyangizni kiriting');
+let yosh = prompt('Yoshingizni kiriting');
+let fan = prompt('Yoqtirga faningizni kiriting');
+let manzil = prompt('Yashaydigan manzilingizni kiriting');
+alert('malumotlaringizni kiritganingiz uchun tashakkur')
+
+
+console.log('ism:', ism);
+console.log('familiya:', familiya);
+console.log('yosh:', yosh);
+console.log('fan:', fan);
+console.log('manzil:', manzil);
